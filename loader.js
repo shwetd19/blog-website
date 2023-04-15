0f31f975965a5415a5bca0d982cbf317b1a1600f
ShwetSd <119885670+shwetd19@users.noreply.github.com>
2023-04-15 10:33:12 +0530

    index file updated with css and js files

--- loader.js
+++
@@ -1,9 +0,0 @@
-const spinnerWrapperEl = document.querySelector('.spinner-wrapper');
-
-    window.addEventListener('load', () => {
-      spinnerWrapperEl.style.opacity = '0';
-
-      setTimeout(() => {
-        spinnerWrapperEl.style.display = 'none';
-      }, 3000);
-    });
